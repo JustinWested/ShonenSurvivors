d344d9758c5c93bb0d3456f6c32b37aee6819176
Justin Wested <justwested@gmail.com>
2025-07-16 23:15:27 -0700

    tinkering with the idea of adding online capability

--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -1,5 +1,7 @@
 extends CanvasLayer
 
+const PORT: int = 3000
+
 var options_scene = preload("res://scenes/ui/options_menu.tscn")
 
 
@@ -8,6 +10,12 @@ func _ready():
 	$%OptionsButton.pressed.connect(on_options_pressed)
 	$%QuitButton.pressed.connect(on_quit_pressed)
 	$%MetaButton.pressed.connect(on_meta_pressed)
+	$%HostButton.pressed.connect(on_host_pressed)
+	$%JoinButton.pressed.connect(on_join_pressed)
+	
+	#NETWORK MULTIPLAYER RELATED CODE BEYOND THIS POINT
+	
+	multiplayer.peer_connected.connect(on_peer_connected)
 	
 
 func on_play_pressed():
@@ -29,12 +37,28 @@ func on_options_closed(options_instance: Node):
 
 func on_quit_pressed():
 	get_tree().quit()
-
+	
 
 func on_meta_pressed():
 	ScreenTransition.transition()
 	await ScreenTransition.transitioned_halfway
 	get_tree().change_scene_to_file("res://scenes/ui/meta_menu.tscn")
 
+
+#NETWORK MULTIPLAYER RELATED CODE BEYOND THIS POINT
+#NETWORK MULTIPLAYER RELATED CODE BEYOND THIS POINT
+
+func on_host_pressed():
+	var server_peer := ENetMultiplayerPeer.new()
+	server_peer.create_server(PORT)
+	multiplayer.multiplayer_peer = server_peer
 	
 	
+func on_join_pressed():
+	var client_peer := ENetMultiplayerPeer.new()
+	client_peer.create_client("127.0.0.1", PORT)
+	multiplayer.multiplayer_peer = client_peer
+	
+	
+func on_peer_connected(id : int):
+	pass
